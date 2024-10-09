5fa5a5ee708adf413c1b252aead6b303ace12e70
imkhateeb <luckykhateeb2@gmail.com>
2024-10-10 03:16:54 +0530

    Small changes

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,10 @@
+// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    ssr: "src/entry-server.jsx",
-    rollupOptions: {
-      input: "./index.html",
-    },
+    outDir: "dist", // Ensure this is set to 'dist'
   },
 });
